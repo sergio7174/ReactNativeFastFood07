d428736f184a6fdc82c22f85e7e7fda24d71b93b
System Eng.Sergio Moncada <94627516+sergio7174@users.noreply.github.com>
2025-09-21 14:45:44 -0400

    Update firebaseConfig.js

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -5,14 +5,14 @@ import 'firebase/auth';
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD3ICjjrEtDQkc7RT6ZHcSSgHZWyrUvPdw",
-  authDomain: "myfirebaseapp-ac660.firebaseapp.com",
-  databaseURL: "https://myfirebaseapp-ac660-default-rtdb.firebaseio.com",
-  projectId: "myfirebaseapp-ac660",
-  storageBucket: "myfirebaseapp-ac660.firebasestorage.app",
-  messagingSenderId: "790642062419",
-  appId: "1:790642062419:web:58a1816f47dd579c35c2a6",
-  measurementId: "G-P241K21LSD"
+  apiKey: 
+  authDomain: 
+  databaseURL: 
+  projectId: 
+  storageBucket: 
+  messagingSenderId: 
+  appId: 
+  measurementId: 
 }
 
 
